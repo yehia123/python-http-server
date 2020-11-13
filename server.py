1d891ac11b674b55d313a58946463ea956c9def2
Yehia Qtaish <yehiaqtaish@gmail.com>
2020-11-13 12:48:59 -0800

    Add files via upload
    Server file using socket

---
+++ server.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+import socket
+
+HOST, PORT = '',8888
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((HOST, PORT))
+listen_socket.listen(1)
+print(f'serving HTTP on {PORT} ...')
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request_data = client_connection.recv(1024)
+    print(request_data.decode('utf-8'))
+
+    http_response = b"""\
+    HTTP/1.1 200 server is connected using python
+    """
+
+    client_connection.sendall(http_response)
+    client_connection.close()
